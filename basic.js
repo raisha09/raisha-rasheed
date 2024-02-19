3e132a0c35b48bdd562d1265e4b5f593dc88febb
Yaseeeen <halfduplex001@gmail.com>
2024-02-19 17:01:53 +0530

    first commit

---
+++ basic.js
@@ -0,0 +1,66 @@
+//let x=10;
+//console.log(x);
+//{
+  //  let x=40;
+    //console.log(x); 
+//}
+//a=5
+//console.log(a); 
+//a=true;
+//console.log(a);
+//console.log(typeof(a));
+ //c=1;
+ //b=c++;
+ //console.log(c)
+// 
+
+// for( let i=0;i<=15;i++)
+// if(i%2==0)
+// {
+//     console.log(i,"is even");
+// }
+// else{
+//     console.log(i,"is odd");
+// }
+ //var fruits=["apple","banana","orange"]
+// console.log(fruits)
+// console.log(fruits.length);
+// fruits[2]="grape";
+// console.log(fruits)
+// for(let i=0;i<fruits.length;i++)
+// {
+//     console.log(fruits[i])
+// }
+//  fruits.push("kiwi");
+//  console.log(fruits);
+//  fruits.pop();
+//  console.log(fruits);
+//  fruits.shift("grape");
+//  console.log(fruits);
+// function greet(name)
+// {
+//     console.log('welcome'+' '+name+"!");
+// }
+// greet('raisha');
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(2,3));
+// 
+// var add=(a,b)=>{
+//     var sum=a+b
+//     console.log(sum)
+//     console.log('sum is ${sum}')
+// }
+// add(10,20);
+// var[a,b,c,d]=["apple","orange","pear","mango"]
+// console.log(a);
+// console.log(b); 
+var a=[1,2,3]
+var b=[4,5,6]
+var c=[...a,...b];
+console.log(c);
+var[a,b,...t]=[1,2,3,4,5,6];
+console.log(t);
+console.log("first element of t is "+t[0]);
+
